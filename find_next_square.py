0e803f0e61a11a92fdd5afc2af2558d9f3dec615
Victor Vidal <victor_vdu@hotmail.com>
2024-06-14 00:02:10 -0600

    Adding a new exercise

--- find_next_square.py
+++ find_next_square.py
@@ -12,5 +12,4 @@ Examples:(Input --> Output)
 '''
 
 def find_next_square(sq):
-    # Return the next square if sq is a square, -1 otherwise
     return (int(sq**0.5)+1)**2 if (sq**0.5).is_integer() else -1
