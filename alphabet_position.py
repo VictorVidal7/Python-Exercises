46a2a46764ab8cf51139269edf5819dd5ceaa1df
Victor Vidal <victor_vdu@hotmail.com>
2024-07-24 00:08:09 -0600

    Exercise Updated

--- alphabet_position.py
+++ alphabet_position.py
@@ -12,3 +12,6 @@ Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 1
 
 def alphabet_position(text):
     return ' '.join(str(ord(c) - 96) for c in text.lower() if c.isalpha())
+
+def alphabet_position(text):
+    return text.replace(' ', '').lower().translate(str.maketrans('abcdefghijklmnopqrstuvwxyz', '1234567891011121314151617181920212223242526'))
