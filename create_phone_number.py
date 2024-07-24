9f48895b99d1b83293b95a572e41a6f10470e558
Victor Vidal <victor_vdu@hotmail.com>
2024-07-24 00:16:18 -0600

    Update

--- create_phone_number.py
+++ create_phone_number.py
@@ -8,3 +8,6 @@ Don't forget the space after the closing parentheses!'''
 
 def create_phone_number(n):
     return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
+
+def create_phone_number(n):
+    return f"({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}"
