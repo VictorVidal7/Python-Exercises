e9f445125a7620f6cddef924bfae57cd1eb6bb92
Victor Vidal <victor_vdu@hotmail.com>
2024-07-24 18:46:12 -0600

    Updated

--- DNA_strand.py
+++ DNA_strand.py
@@ -2,7 +2,8 @@
 
 If you want to know more: http://en.wikipedia.org/wiki/DNA
 
-In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); 
+you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
 
 More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
 
@@ -14,3 +15,7 @@ Example: (input --> output)
 
 def DNA_strand(dna):
     return dna.translate(str.maketrans('ATCG', 'TAGC'))
+
+def DNA_strand(dna):
+    dna = dna.replace('A', 't').replace('T', 'a').replace('C', 'g').replace('G', 'c')
+    return dna.upper()
