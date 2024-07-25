02ca1d5e5797e63138fbcf316fc199d24f1179d4
Victor Vidal <victor_vdu@hotmail.com>
2024-07-24 20:31:50 -0600

    Update

--- find_outlier.py
+++ find_outlier.py
@@ -1,4 +1,6 @@
-'''You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+'''You are given an array (which will have a length of at least 3, but could be very large) containing integers. 
+The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. 
+Write a method that takes the array as an argument and returns this "outlier" N.
 
 Examples
 [2, 4, 0, 100, 4, 11, 2602, 36] -->  11 (the only odd number)
@@ -6,6 +8,9 @@ Examples
 [160, 3, 1719, 19, 11, 13, -21] --> 160 (the only even number)'''
 
 def find_outlier(integers):
-    odd = [x for x in integers if x % 2 != 0]
-    even = [x for x in integers if x % 2 == 0]
-    return odd[0] if len(odd) == 1 else even[0]
+    evens = [i for i in integers if i % 2 == 0]
+    odds = [i for i in integers if i % 2 == 1]
+    return evens[0] if len(evens) == 1 else odds[0]
+
+print(find_outlier([2, 4, 0, 100, 4, 11, 2602, 36])) # [11]
+print(find_outlier([160, 3, 1719, 19, 11, 13, -21])) # [160]
