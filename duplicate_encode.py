8609ad8717cf745c0ea5341c4147678e6730b5b3
Victor Vidal <victor_vdu@hotmail.com>
2024-07-24 19:34:52 -0600

    Updated

--- duplicate_encode.py
+++ duplicate_encode.py
@@ -1,4 +1,6 @@
-'''The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+'''The goal of this exercise is to convert a string to a new string where each character in the new string is "(" 
+if that character appears only once in the original string, or ")" if that character appears more than once in the original string. 
+Ignore capitalization when determining if a character is a duplicate.
 
 Examples
 "din"      =>  "((("
@@ -6,14 +8,36 @@ Examples
 "Success"  =>  ")())())"
 "(( @"     =>  "))((" 
 Notes
-Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!'''
+Assertion messages may be unclear about what they display in some languages. If you read "...
+It Should encode XXX", the "XXX" is the expected result, not the input!'''
 
 def duplicate_encode(word):
     word = word.lower()
-    result = ''
-    for i in word:
-        if word.count(i) > 1:
-            result += ')'
-        else:
+    result = ""
+    for w in word:
+        if word.count(w) == 1:
             result += '('
+        else:
+            result += ')'
     return result
+
+def duplicate_encode(word):
+    word = word.lower()
+    dict_freq = {}
+    for w in word:
+        if w in dict_freq:
+            dict_freq[w] = dict_freq[w] + 1
+        else:
+            dict_freq[w] = 1
+    
+    result = ""
+    for w in word:
+        if dict_freq[w] == 1: 
+            result += '('
+        else: 
+            result += ')'
+    return result
+
+def duplicate_encode(word):
+    #shortest solution
+    return "".join(['(' if word.lower().count(w) == 1 else ')' for w in word.lower()])            
