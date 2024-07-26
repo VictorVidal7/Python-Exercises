c658134313e065a91bd9bc8571a4678d1be88982
Victor Vidal <victor_vdu@hotmail.com>
2024-07-25 22:32:18 -0600

    Update

--- is_isogram.py
+++ is_isogram.py
@@ -1,4 +1,6 @@
-'''An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+'''An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. 
+Ignore letter case.
 
 Example: (Input --> Output)
 
@@ -10,4 +12,17 @@ isIsogram "aba" = false
 '''
 
 def is_isogram(string):
+    string = string.lower()
+    for i in string:
+        if string.count(i) > 1:
+            return False
+    return True
+
+def is_isogram(string):
+    #Shortest way
     return len(string) == len(set(string.lower()))
+
+print(is_isogram("Dermatoglyphics")) # True
+print(is_isogram("aba")) # False
+print(is_isogram("moOse")) # False
+
--- is_pangram.py
+++ is_pangram.py
@@ -1,7 +1,25 @@
-'''A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+'''A pangram is a sentence that contains every single letter of the alphabet at least once. 
+For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, 
+because it uses the letters A-Z at least once (case is irrelevant).
 
-Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.'''
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
 
+Example: (Input --> Output)
+"The quick brown fox jumps over the lazy dog." --> True
+"This is not a pangram." --> False
+'''
+import string
+def is_pangram(st):
+    for i in string.ascii_lowercase:
+        if i not in st.lower():
+            return False
+    return True
 
 def is_pangram(st):
-    return all(letter in st.lower() for letter in 'abcdefghijklmnopqrstuvwxyz')
+    return [True if all(i in st.lower() for i in string.ascii_lowercase) else False][0]
+
+def is_pangram(st):
+    return set(string.ascii_lowercase) <= set(st.lower())
+
+print(is_pangram("The quick brown fox jumps over the lazy dog.")) # True
+print(is_pangram("This is not a pangram.")) # False
