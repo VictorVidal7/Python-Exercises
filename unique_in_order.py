97167bdbce8cba5e63860f898c332e8ec20c06c5
Victor Vidal <victor_vdu@hotmail.com>
2024-07-26 01:52:45 -0600

    Updated

--- unique_in_order.py
+++ unique_in_order.py
@@ -1,4 +1,5 @@
-'''Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+'''Implement the function unique_in_order which takes as argument a sequence and returns a list of items without 
+any elements with the same value next to each other and preserving the original order of elements.
 
 For example:
 
@@ -7,6 +8,18 @@ unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 unique_in_order([1, 2, 2, 3, 3])   == [1, 2, 3]
 unique_in_order((1, 2, 2, 3, 3))   == [1, 2, 3]'''
 
+def unique_in_order(sequence):
+    if len(sequence) == 0:
+        return []
+    else:
+        new = []
+        for i in range(len(sequence)):
+            if (i+1) < len(sequence):
+                if sequence[i] != sequence[i+1]:
+                    new.append(sequence[i])
+            else:
+                new.append(sequence[i])
+        return new
 
 def unique_in_order(sequence):
     if len(sequence) == 0:
@@ -17,3 +30,10 @@ def unique_in_order(sequence):
             if sequence[i] != sequence[i-1]:
                 unique.append(sequence[i])
         return unique
+    
+def unique_in_order(sequence):
+    return [sequence[i] for i in range(len(sequence)) if i == 0 or sequence[i] != sequence[i-1]]
+    
+print(unique_in_order('AAAABBBCCDAABBB')) # ['A', 'B', 'C', 'D', 'A', 'B'
+print(unique_in_order('ABBCcAD')) # ['A', 'B', 'C', 'c', 'A', 'D']
+print(unique_in_order([1, 2, 2, 3, 3])) # [1, 2, 3]
