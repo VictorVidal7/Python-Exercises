d1d3f8994e87273f3f82549506881b459974b0ef
Victor Vidal <victor_vdu@hotmail.com>
2024-07-25 19:02:20 -0600

    Update

--- get_sum.py
+++ get_sum.py
@@ -22,8 +22,5 @@ def get_sum(a,b):
     else:
         return sum(range(b, a+1))
     
-
-print(list(range(1, 5)))
-
-print(get_sum(1, 0)) # 1
-print(get_sum(1, 2)) # 3
+def get_sum(a,b):
+    return sum(range(min(a, b), max(a, b) + 1))
