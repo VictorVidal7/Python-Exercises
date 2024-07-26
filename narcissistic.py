d85bc9e58b10d8c1a30738408b81b5b50604cb67
Victor Vidal <victor_vdu@hotmail.com>
2024-07-26 00:12:52 -0600

    Update

--- narcissistic.py
+++ narcissistic.py
@@ -1,4 +1,5 @@
-'''A Narcissistic Number (or Armstrong Number) is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+'''A Narcissistic Number (or Armstrong Number) is a positive number which is the sum of its own digits, 
+each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
 
 For example, take 153 (3 digits), which is narcissistic:
 
@@ -12,8 +13,22 @@ Your code must return true or false (not 'true' and 'false') depending upon whet
 
 This may be True and False in your language, e.g. PHP.
 
-Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.'''
+Error checking for text strings or other invalid inputs is not required, 
+only valid positive non-zero integers will be passed into the function.
+
+Example:
+
+narcissistic(153) # returns True
+narcissistic(1652) # returns False
+narcissistic(371) # returns True
+narcissistic(122) # returns False
+'''
 
 
 def narcissistic(value):
     return value == sum([int(i) ** len(str(value)) for i in str(value)])
+
+print(narcissistic(153)) # True
+print(narcissistic(1652)) # False
+print(narcissistic(371)) # True
+print(narcissistic(122)) # False
