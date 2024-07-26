b8e1389720f6f655065217889e2d98047b9ed90b
Victor Vidal <victor_vdu@hotmail.com>
2024-07-26 00:01:33 -0600

    Update

--- maskify.py
+++ maskify.py
@@ -1,4 +1,6 @@
-'''Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+'''Usually when you buy something, you're asked whether your credit card number, 
+phone number or answer to your most secret question is still correct. 
+However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
 
 Your task is to write a function maskify, which changes all but the last four characters into '#'.
 
@@ -13,6 +15,10 @@ Examples (input --> output):
 "Nananananananananananananananana Batman!" --> "####################################man!"
 '''
 
-
 def maskify(cc):
     return '#'*(len(cc)-4) + cc[-4:]
+
+print(maskify("4556364607935616")) # "############5616"
+print(maskify("64607935616")) # "#######5616"
+print(maskify("1")) # "1"
+print(maskify("")) # ""
