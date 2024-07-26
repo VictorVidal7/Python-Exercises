927395df2368d654c7a6066e984c9d35704e610e
Victor Vidal <victor_vdu@hotmail.com>
2024-07-26 01:28:21 -0600

    Update

--- sum_of_multiples.py
+++ sum_of_multiples.py
@@ -4,10 +4,22 @@ Finish the solution so that it returns the sum of all the multiples of 3 or 5 be
 
 Additionally, if the number is negative, return 0.
 
-Note: If the number is a multiple of both 3 and 5, only count it once.'''
+Note: If the number is a multiple of both 3 and 5, only count it once.
+
+Examples:
+solution(10) # 23
+solution(20) # 78
+solution(-5) # 0'''
 
 def solution(number):
     if number < 0:
         return 0
     else:
         return sum([i for i in range(number) if i % 3 == 0 or i % 5 == 0])
+    
+def solution(number):
+    return sum([i for i in range(number) if i % 3 == 0 or i % 5 == 0]) if number > 0 else 0
+    
+print(solution(10)) # 23
+print(solution(20)) # 78
+print(solution(-5)) # 0
