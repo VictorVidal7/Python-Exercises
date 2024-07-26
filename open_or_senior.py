7a432f870956905ebfea4e47fcc3d29b51392115
Victor Vidal <victor_vdu@hotmail.com>
2024-07-26 00:28:19 -0600

    Update

--- open_or_senior.py
+++ open_or_senior.py
@@ -14,3 +14,11 @@ output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]'''
 
 def open_or_senior(data):
     return ['Senior' if age >= 55 and handicap > 7 else 'Open' for age, handicap in data]
+
+def open_or_senior(data):
+    result = []
+    for i in data:
+        if i[0] >= 55 and i[1] > 7:
+            result.append('Senior')
+        else:
+            result.append('Open')
